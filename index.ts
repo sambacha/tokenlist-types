f2879ad77a3cced9ee2127c4f4b954a527226ed1
sam <sam@freighttrust.com>
2021-04-14 13:40:26 -0700

    0.1.0

---
+++ index.ts
@@ -0,0 +1,10 @@
+/**
+ *  @tokenlists 
+ *  @param TokenList
+ *  @returns types
+ *  @version 0.1.0
+ */
+import schema from "./token.d.ts";
+export namespace schema {}
+import TokenList from "./types.d.ts";
+export default TokenList
